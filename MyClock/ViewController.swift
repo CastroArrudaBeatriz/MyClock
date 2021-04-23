69d357774f0c0f64ead01db851ed969d797bf80c
Beatriz Castro <beatriz_arruda_castro@hotmail.com>
2021-04-23 00:14:08 -0300

    Initial Commit

---
+++ MyClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyClock
+//
+//  Created by Beatriz Castro on 23/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
