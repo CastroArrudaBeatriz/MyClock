29036f0c924f2bae180ea0d17e38c7854f5aeda5
Beatriz Castro <beatriz_arruda_castro@hotmail.com>
2021-04-23 00:59:47 -0300

    iniciar layout

--- MyClock/ViewController.swift
+++ MyClock/ViewController.swift
@@ -7,11 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerSeconds: UIPickerView!
+    
+    var pickerData: [String] = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerSeconds.delegate = self
+        pickerSeconds.dataSource = self
+        
+        pickerData = ["10 segundos",
+                      "20 segundos",
+                      "30 segundos",
+                      "40 segundos",
+                      "50 segundos",
+                      "60 segundos"]
+        
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 
 
